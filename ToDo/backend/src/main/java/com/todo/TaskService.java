8a2b354edec8f82fdc34b485685e40f39185fec7
Harsh sinha <164148903+Harshsinha07@users.noreply.github.com>
2024-12-09 15:06:35 +0530

    Add files via upload

---
+++ ToDo/backend/src/main/java/com/todo/TaskService.java
@@ -0,0 +1,28 @@
+package com.todo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task addTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public Task updateTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+}
